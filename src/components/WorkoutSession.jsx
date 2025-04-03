cc734022d8bae7d4d234725a8dc5d730e1b2f33c
Dheeraj <dheerajsonkarmy@gmail.com>
2025-04-03 14:25:27 +0530

    gym

---
+++ src/components/WorkoutSession.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const WorkoutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+        A top workout session includes warm-up, strength training, cardio, core exercises, flexibility, intensity, consistency, hydration, and recovery.
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+        Feature bootcamps offer intensive, hands-on training in product management, design, and tech skills, preparing professionals for industry roles.
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSessions;
